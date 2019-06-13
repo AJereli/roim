2b06859b3807d6527ac68e0175aa7ac04d1b7d41
Anatoliy <anatoliy.v@thrive.io>
2019-06-14 01:26:47 +0300

    upd

--- CNN.py
+++ CNN.py
@@ -14,6 +14,9 @@ import matplotlib.pyplot as plt
 train_data = train_data.reshape((60000, 28, 28, 1))
 train_data = train_data.astype('float32') / 255
 
+test_data = test_data.reshape((10000, 28, 28, 1))
+test_data = test_data.astype('float32') / 255
+
 train_labels = to_categorical(train_labels)
 test_labels = to_categorical(test_labels)
 
