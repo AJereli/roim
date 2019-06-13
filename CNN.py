479dd2a74bf7dac7c94af3829f95675e7d6475d9
Anatoliy <anatoliy.v@thrive.io>
2019-06-14 01:02:15 +0300

    cnn and upd

---
+++ CNN.py
@@ -0,0 +1,46 @@
+from keras.datasets import mnist
+from keras.utils import to_categorical
+from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
+from keras import models
+from keras.callbacks import ReduceLROnPlateau, EarlyStopping, TensorBoard
+
+import matplotlib.pyplot as plt
+
+(train_data, train_labels), (test_data, test_labels) = mnist.load_data()
+
+# plt.imshow(train_data[1])
+# plt.show()
+
+train_data = train_data.reshape((60000, 28, 28, 1))
+train_data = train_data.astype('float32') / 255
+
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+model = models.Sequential()
+
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(MaxPooling2D(2, 2))
+model.add(Conv2D(64, (3,3), activation='relu'))
+model.add(MaxPooling2D(2, 2))
+model.add(Conv2D(64, (3,3), activation='relu'))
+model.add(Flatten())
+model.add(Dense(64, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+tensorboard = TensorBoard(log_dir='./logs/cnn', histogram_freq=0,
+                          write_graph=True, write_images=False)
+print(model.summary())
+EPOCHS = 30
+BATCH_SIZE = 64
+history = model.fit(train_data, train_labels,
+                    batch_size=BATCH_SIZE,
+                    epochs=EPOCHS,
+                    verbose=0,
+                    callbacks=[tensorboard])
+
+score = model.evaluate(test_data, test_labels, batch_size=BATCH_SIZE)
+print()
+print("ACCURACY:",score[1])
+print("LOSS:",score[0])
--- text_classification_RNN.py
+++ text_classification_RNN.py
@@ -80,8 +80,7 @@ x_test = pad_sequences(tokenizer.texts_to_sequences(df_test.text), maxlen=SEQUEN
 y_train = np.array(df_train.target.tolist())
 y_test = np.array(df_test.target.tolist())
 
-y_train = y_train.reshape(-1,1)
-y_test = y_test.reshape(-1,1)
+
 print("x_train", x_train.shape)
 print("y_train", y_train.shape)
 print()
